e1467c45b255cc526d62ee807b7be952ab4ebef5
iokl1004 <iokl3319@naver.com>
2024-10-04 19:52:38 +0900

    20241004
    1~3

---
+++ 1. Dev/boilerplate-mern-stack-master/client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+// 웹 사이트를 만들 때 수월하게 항상 켜지 않아도 이미지와 영화의 정보들을 가져 올 수 있음.
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.thmdb.org/t/p/'
